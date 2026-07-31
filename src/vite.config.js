ca887f79c8c4017c6c97eff9b7082ddbd92346c5
yoshidasyoki <yoshikingcorporation@gmail.com>
2026-07-31 22:50:16 +0900

    Tailwind CSSが使えるよう設定追加

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 import laravel from 'laravel-vite-plugin';
 import { bunny } from 'laravel-vite-plugin/fonts';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
@@ -19,6 +19,11 @@ export default defineConfig({
     server: {
         watch: {
             ignored: ['**/storage/framework/views/**'],
+        },
+        host: '0.0.0.0',
+        port: 5173,
+        hmr: {
+            host: 'localhost',
         },
     },
 });
